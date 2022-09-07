b2e71e3c591edd8f4f4f4cb1eecc1067623a9622
Bernd Hirschmann <bernd@guidnew.com>
2022-09-07 13:12:32 +0200

    allow up to 6 input selectors

--- Fluxor.Selectors/MemoizedSelector.cs
+++ Fluxor.Selectors/MemoizedSelector.cs
@@ -140,6 +140,12 @@ public class MemoizedSelector<TIn1, TIn2, TResult> : MemoizedSelector<TIn1, TRes
         _inS2 = inS2;
     }
 
+    public override void Release()
+    {
+        base.Release();
+        InS2LastResult = null;
+    }
+
     protected override TResult CallProjectorFunc(IStore state)
     {
         if (_projectorFunc == null ||
@@ -193,6 +199,12 @@ public class MemoizedSelector<TIn1, TIn2, TIn3, TResult> : MemoizedSelector<TIn1
         _inS3 = inS3;
     }
 
+    public override void Release()
+    {
+        base.Release();
+        InS3LastResult = null;
+    }
+
     protected override TResult CallProjectorFunc(IStore state)
     {
         if (_projectorFunc == null ||
@@ -217,6 +229,219 @@ public class MemoizedSelector<TIn1, TIn2, TIn3, TResult> : MemoizedSelector<TIn1
         if (hasChanges)
         {
             InS3LastResult = result;
+        }
+
+        return hasBaseChanges || hasChanges;
+    }
+}
+
+public class MemoizedSelector<TIn1, TIn2, TIn3, TIn4, TResult> : MemoizedSelector<TIn1, TIn2, TIn3, TResult>
+{
+    protected SelectorResult<TIn4>? InS4LastResult { get; private set; }
+
+    private ISelector<TIn4> _inS4;
+
+    private Func<TIn1, TIn2, TIn3, TIn4, TResult>? _projectorFunc;
+
+    public MemoizedSelector(
+        ISelector<TIn1> inS1,
+        ISelector<TIn2> inS2,
+        ISelector<TIn3> inS3,
+        ISelector<TIn4> inS4,
+        Func<TIn1, TIn2, TIn3, TIn4, TResult> projectorFunc)
+        : this(inS1, inS2, inS3, inS4)
+    {
+        _projectorFunc = projectorFunc;
+    }
+
+    protected MemoizedSelector(
+        ISelector<TIn1> inS1,
+        ISelector<TIn2> inS2,
+        ISelector<TIn3> inS3,
+        ISelector<TIn4> inS4)
+        : base(inS1, inS2, inS3)
+    {
+        _inS4 = inS4;
+    }
+
+    public override void Release()
+    {
+        base.Release();
+        InS4LastResult = null;
+    }
+
+    protected override TResult CallProjectorFunc(IStore state)
+    {
+        if (_projectorFunc == null ||
+            InS1LastResult == null ||
+            InS2LastResult == null ||
+            InS3LastResult == null ||
+            InS4LastResult == null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return _projectorFunc(
+            InS1LastResult.Result,
+            InS2LastResult.Result,
+            InS3LastResult.Result,
+            InS4LastResult.Result);
+    }
+
+    protected override bool UpdateInputResults(IStore state)
+    {
+        bool hasBaseChanges = base.UpdateInputResults(state);
+
+        var result = UpdateInputResult(state, _inS4, InS4LastResult, out var hasChanges);
+        if (hasChanges)
+        {
+            InS4LastResult = result;
+        }
+
+        return hasBaseChanges || hasChanges;
+    }
+}
+
+public class MemoizedSelector<TIn1, TIn2, TIn3, TIn4, TIn5, TResult> : MemoizedSelector<TIn1, TIn2, TIn3, TIn4, TResult>
+{
+    protected SelectorResult<TIn5>? InS5LastResult { get; private set; }
+
+    private ISelector<TIn5> _inS5;
+
+    private Func<TIn1, TIn2, TIn3, TIn4, TIn5, TResult>? _projectorFunc;
+
+    public MemoizedSelector(
+        ISelector<TIn1> inS1,
+        ISelector<TIn2> inS2,
+        ISelector<TIn3> inS3,
+        ISelector<TIn4> inS4,
+        ISelector<TIn5> inS5,
+        Func<TIn1, TIn2, TIn3, TIn4, TIn5, TResult> projectorFunc)
+        : this(inS1, inS2, inS3, inS4, inS5)
+    {
+        _projectorFunc = projectorFunc;
+    }
+
+    protected MemoizedSelector(
+        ISelector<TIn1> inS1,
+        ISelector<TIn2> inS2,
+        ISelector<TIn3> inS3,
+        ISelector<TIn4> inS4,
+        ISelector<TIn5> inS5)
+        : base(inS1, inS2, inS3, inS4)
+    {
+        _inS5 = inS5;
+    }
+
+    public override void Release()
+    {
+        base.Release();
+        InS5LastResult = null;
+    }
+
+    protected override TResult CallProjectorFunc(IStore state)
+    {
+        if (_projectorFunc == null ||
+            InS1LastResult == null ||
+            InS2LastResult == null ||
+            InS3LastResult == null ||
+            InS4LastResult == null ||
+            InS5LastResult == null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return _projectorFunc(
+            InS1LastResult.Result,
+            InS2LastResult.Result,
+            InS3LastResult.Result,
+            InS4LastResult.Result,
+            InS5LastResult.Result);
+    }
+
+    protected override bool UpdateInputResults(IStore state)
+    {
+        bool hasBaseChanges = base.UpdateInputResults(state);
+
+        var result = UpdateInputResult(state, _inS5, InS5LastResult, out var hasChanges);
+        if (hasChanges)
+        {
+            InS5LastResult = result;
+        }
+
+        return hasBaseChanges || hasChanges;
+    }
+}
+
+public class MemoizedSelector<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TResult> : MemoizedSelector<TIn1, TIn2, TIn3, TIn4, TIn5, TResult>
+{
+    protected SelectorResult<TIn6>? InS6LastResult { get; private set; }
+
+    private ISelector<TIn6> _inS6;
+
+    private Func<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TResult>? _projectorFunc;
+
+    public MemoizedSelector(
+        ISelector<TIn1> inS1,
+        ISelector<TIn2> inS2,
+        ISelector<TIn3> inS3,
+        ISelector<TIn4> inS4,
+        ISelector<TIn5> inS5,
+        ISelector<TIn6> inS6,
+        Func<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TResult> projectorFunc)
+        : this(inS1, inS2, inS3, inS4, inS5, inS6)
+    {
+        _projectorFunc = projectorFunc;
+    }
+
+    protected MemoizedSelector(
+        ISelector<TIn1> inS1,
+        ISelector<TIn2> inS2,
+        ISelector<TIn3> inS3,
+        ISelector<TIn4> inS4,
+        ISelector<TIn5> inS5,
+        ISelector<TIn6> inS6)
+        : base(inS1, inS2, inS3, inS4, inS5)
+    {
+        _inS6 = inS6;
+    }
+
+    public override void Release()
+    {
+        base.Release();
+        InS6LastResult = null;
+    }
+
+    protected override TResult CallProjectorFunc(IStore state)
+    {
+        if (_projectorFunc == null ||
+            InS1LastResult == null ||
+            InS2LastResult == null ||
+            InS3LastResult == null ||
+            InS4LastResult == null ||
+            InS5LastResult == null ||
+            InS6LastResult == null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return _projectorFunc(
+            InS1LastResult.Result,
+            InS2LastResult.Result,
+            InS3LastResult.Result,
+            InS4LastResult.Result,
+            InS5LastResult.Result,
+            InS6LastResult.Result);
+    }
+
+    protected override bool UpdateInputResults(IStore state)
+    {
+        bool hasBaseChanges = base.UpdateInputResults(state);
+
+        var result = UpdateInputResult(state, _inS6, InS6LastResult, out var hasChanges);
+        if (hasChanges)
+        {
+            InS6LastResult = result;
         }
 
         return hasBaseChanges || hasChanges;
--- Fluxor.Selectors/SelectorFactory.cs
+++ Fluxor.Selectors/SelectorFactory.cs
@@ -23,6 +23,39 @@ public static class SelectorFactory
         return new MemoizedSelector<TIn1, TIn2, TIn3, TResult>(inS1, inS2, inS3, projector);
     }
 
+    public static MemoizedSelector<TIn1, TIn2, TIn3, TIn4, TResult> CreateSelector<TIn1, TIn2, TIn3, TIn4, TResult>(
+        ISelector<TIn1> inS1,
+        ISelector<TIn2> inS2,
+        ISelector<TIn3> inS3,
+        ISelector<TIn4> inS4,
+        Func<TIn1, TIn2, TIn3, TIn4, TResult> projector)
+    {
+        return new MemoizedSelector<TIn1, TIn2, TIn3, TIn4, TResult>(inS1, inS2, inS3, inS4, projector);
+    }
+
+    public static MemoizedSelector<TIn1, TIn2, TIn3, TIn4, TIn5, TResult> CreateSelector<TIn1, TIn2, TIn3, TIn4, TIn5, TResult>(
+        ISelector<TIn1> inS1,
+        ISelector<TIn2> inS2,
+        ISelector<TIn3> inS3,
+        ISelector<TIn4> inS4,
+        ISelector<TIn5> inS5,
+        Func<TIn1, TIn2, TIn3, TIn4, TIn5, TResult> projector)
+    {
+        return new MemoizedSelector<TIn1, TIn2, TIn3, TIn4, TIn5, TResult>(inS1, inS2, inS3, inS4, inS5, projector);
+    }
+
+    public static MemoizedSelector<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TResult> CreateSelector<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TResult>(
+        ISelector<TIn1> inS1,
+        ISelector<TIn2> inS2,
+        ISelector<TIn3> inS3,
+        ISelector<TIn4> inS4,
+        ISelector<TIn5> inS5,
+        ISelector<TIn6> inS6,
+        Func<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TResult> projector)
+    {
+        return new MemoizedSelector<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TResult>(inS1, inS2, inS3, inS4, inS5, inS6, projector);
+    }
+
     public static Selector<TFeatureState> CreateFeatureSelector<TFeatureState>(string name)
     {
         return new Selector<TFeatureState>(store =>
